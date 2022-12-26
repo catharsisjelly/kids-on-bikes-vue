85fb002dfbc9985be835bf50e9af1b8aebc2f7d3
Chris Lock <code@catharsis.co.uk>
2022-12-26 21:28:56 +0000

    Lint and fix

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import InputTest from './components/DiceRoller.vue';
+import InputTest from './components/DiceRoller.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-      <InputTest />
+    <header>
+        <div class="wrapper">
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+            </nav>
+            <InputTest />
+        </div>
+    </header>
 
-    </div>
-  </header>
-
-  <RouterView />
+    <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/components/DiceRoller.vue
+++ src/components/DiceRoller.vue
@@ -7,16 +7,16 @@ import { DiceRoll } from '@dice-roller/rpg-dice-roller'
 const numberOfDice = ref(1)
 const playerResults = ref()
 const computerResult = ref()
-const error = ref('');
+const error = ref('')
 
 const roll = () => {
-    error.value = '';
+    error.value = ''
 
     try {
         playerResults.value = new DiceRoll(`${unref(numberOfDice)}d6>=6`)
         computerResult.value = new DiceRoll(`1d6>=6`)
     } catch (e: any) {
-        error.value = `There was an error` 
+        error.value = `There was an error`
     }
 }
 </script>
@@ -25,24 +25,39 @@ const roll = () => {
     <div class="grid formgrid">
         <div class="field">
             <label for="numberOfDice">Number of Dice to roll</label>
-            <InputNumber 
-                inputId="numberOfDice" 
+            <InputNumber
+                inputId="numberOfDice"
                 v-model="numberOfDice"
                 mode="decimal"
-                min="1" 
+                min="1"
             />
         </div>
     </div>
     <Button label="Roll" @click="roll()" />
     <div v-if="playerResults">
         <div>
-            <p>You got {{ playerResults?.rolls[0].value }} Success{{ playerResults?.rolls[0].value == 1 ? '' : 'es' }}
+            <p>
+                You got {{ playerResults?.rolls[0].value }} Success{{
+                    playerResults?.rolls[0].value == 1 ? '' : 'es'
+                }}
             </p>
             <ul>
-                <li v-for="(roll, index) in playerResults?.rolls[0].rolls">
-                    Result: {{ roll.value }} - {{ roll.calculationValue == 1 ? 'Success' : 'Fail' }}</li>
+                <li
+                    v-for="(roll, index) in playerResults?.rolls[0].rolls"
+                    :key="index"
+                >
+                    Result: {{ roll.value }} -
+                    {{ roll.calculationValue == 1 ? 'Success' : 'Fail' }}
+                </li>
             </ul>
         </div>
-        <div>Friend Computer {{ computerResult.rolls[0].value == 1 ? `Noticed you` : `Didn't notice you` }}</div>
+        <div>
+            Friend Computer
+            {{
+                computerResult.rolls[0].value == 1
+                    ? `Noticed you`
+                    : `Didn't notice you`
+            }}
+        </div>
     </div>
 </template>
