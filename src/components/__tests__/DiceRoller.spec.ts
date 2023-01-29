fd9361140a6b7d7093b62ae074869c57797518fc
Chris Lock <code@catharsis.co.uk>
2023-01-29 23:47:27 +0000

    Fixing tests?

--- src/components/__tests__/DiceRoller.spec.ts
+++ src/components/__tests__/DiceRoller.spec.ts
@@ -1,18 +1,24 @@
 import { describe, it, expect } from 'vitest'
-
+import { createTestingPinia } from '@pinia/testing'
 import { mount } from '@vue/test-utils'
 import DiceRoller from '@/components/DiceRoller.vue'
 
 describe('Dice Roller', () => {
     it('Displays the Form', async () => {
-        const wrapper = mount(<DiceRoller notation="d6" />)
+        const wrapper = mount(DiceRoller, {
+            global: { plugins: [createTestingPinia({ createSpy: fn })] },
+            props: {},
+            notation: "d6"
+        })
 
         const rollResults = wrapper.find('[data-test-id=rollResults]')
         expect(rollResults.exists()).toBe(false)
     })
 
     it('Displays the player results after clicking Roll', async () => {
-        const wrapper = mount(<DiceRoller notation="d6" />)
+        const wrapper = mount(DiceRoller, {
+            notation: "d6"
+        })
         await wrapper.get('button').trigger('click')
 
         const rollResults = wrapper.get('[data-test-id=rollResults]')
