693bcca27bca437b5abd912d0896aab300d8bf2c
Chris Lock <code@catharsis.co.uk>
2023-02-01 15:33:56 +0000

    Playwrite

---
+++ tests/basic.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test'
+
+test('should allow me to add todo items', async ({ page }) => {
+    await page.goto('/')
+    
+    await expect(page.locator('h1')).toHaveText('Kids on Bikes Helper')
+})
