d6827791774daf9905ec0b2a5ec840987b64bfc7
Chris Lock <code@catharsis.co.uk>
2023-04-03 10:37:15 +0100

    User the character object

--- src/components/CharacterSheet/PlayerDetails.vue
+++ src/components/CharacterSheet/PlayerDetails.vue
@@ -7,7 +7,7 @@ import { useCharacterSheet } from '@/stores/characterSheet'
 import { storeToRefs } from 'pinia'
 
 const store = useCharacterSheet()
-const { characterDetails } = storeToRefs(store)
+const { character } = storeToRefs(store)
 </script>
 
 <template>
@@ -15,27 +15,27 @@ const { characterDetails } = storeToRefs(store)
     <Fieldset legend="Character Details">
       <div>
         <label for="name" class="col-fixed">Name</label>
-        <InputText id="name" type="text" v-model="characterDetails.name" />
+        <InputText id="name" type="text" v-model="character.name" />
       </div>
       <div>
         <label for="age">Age</label>
-        <InputNumber id="age" v-model="characterDetails.age" />
+        <InputNumber id="age" v-model="character.age" />
       </div>
       <div>
         <label for="fear">Fear</label>
-        <InputText id="fear" type="text" v-model="characterDetails.fear" />
+        <InputText id="fear" type="text" v-model="character.fear" />
       </div>
       <div>
         <label for="motivation">Motivation</label>
-        <InputText id="motivation" type="text" v-model="characterDetails.motivation" />
+        <InputText id="motivation" type="text" v-model="character.motivation" />
       </div>
       <div>
         <label for="flaws">Flaws</label>
-        <InputText id="flaws" type="text" v-model="characterDetails.flaws" />
+        <InputText id="flaws" type="text" v-model="character.flaws" />
       </div>
       <div>
         <label for="description">Description</label>
-        <Textarea id="description" v-model="characterDetails.description" />
+        <Textarea id="description" v-model="character.description" />
       </div>
     </Fieldset>
   </div>
