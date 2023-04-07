5be4ac7ee2e6acd7e1d4c1b486fac537b828c8e6
Chris Lock <code@catharsis.co.uk>
2023-04-07 10:00:08 +0000

    Update Stats.spec.ts and adversityTokens.ts

--- src/stores/adversityTokens.ts
+++ src/stores/adversityTokens.ts
@@ -2,7 +2,7 @@ import { ref, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useAdversityTokens = defineStore('adversityTokens', () => {
-  const adversityTokens = ref(0)
+  const adversityTokens: Ref<number> = ref(0)
 
   return {
     adversityTokens
