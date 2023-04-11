8b9de0d0b89cd6ad003322c7f193f77d579327a0
Chris Lock <code@catharsis.co.uk>
2023-04-11 13:53:33 +0100

    Adding individual dialog display values

--- src/components/CharacterSheet/PlayerStrengths.vue
+++ src/components/CharacterSheet/PlayerStrengths.vue
@@ -2,19 +2,42 @@
 import Fieldset from 'primevue/fieldset'
 import { useCharacterSheet } from '@/stores/characterSheet'
 import strengthsAvailable from '../../data/strengths.json'
-import { ref } from 'vue'
+import { ref, type Ref } from 'vue'
 import Dialog from 'primevue/dialog'
 import Checkbox from 'primevue/checkbox'
 import InputText from 'primevue/inputtext'
 import { storeToRefs } from 'pinia'
+import { strengthValues, type StrengthValue } from '@/lib/Stats'
 
 const store = useCharacterSheet()
 const { strengths } = storeToRefs(store)
 
-const display: any = ref(false)
+const displayDialogs: Ref<Record<StrengthValue, boolean>> = ref({
+  'cool-under-pressure': false,
+  easygoing: false,
+  gross: false,
+  heroic: false,
+  intuitive: false,
+  loyal: false,
+  prepared: false,
+  protective: false,
+  'quick-healing': false,
+  rebellious: false,
+  'skilled-at': false,
+  tough: false,
+  'treasure-hunter': false,
+  unassuming: false,
+  wealthy: false
+})
 
-const openDialog = () => {
-  display.value = true
+const openDialog = (item: string) => {
+  for (const key of strengthValues) {
+    if (key === item) {
+      displayDialogs.value[key] = true
+    } else {
+      displayDialogs.value[key] = false
+    }
+  }
 }
 </script>
 
@@ -24,20 +47,15 @@ const openDialog = () => {
       <Fieldset legend="Strengths">
         <div v-for="(strength, index) in strengthsAvailable" :key="index">
           <label :for="'strength-' + index">{{ strength.label }}</label>
-          <Checkbox
-            v-model="strengths"
-            :value="index"
-            name="strength"
-            :inputId="'strength-' + index"
-          />
+          <Checkbox v-model="strengths" :value="index" name="strength" :inputId="'strength-' + index" />
           <div v-if="strengths.includes('skilled-at') && index === 'skilled-at'">
             <label for="skilled-at-skill">Skilled At skill</label>
             <InputText name="skilled-at-skill" />
           </div>
           <div>
-            <i class="pi pi-info-circle" @click="openDialog"></i>
+            <i class="pi pi-info-circle" @click="openDialog(index)"></i>
           </div>
-          <Dialog :header="strength.label" v-model:visible="display">
+          <Dialog :header="strength.label" v-model:visible="displayDialogs[index]">
             <div>{{ strength.description }}</div>
           </Dialog>
         </div>
