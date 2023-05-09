6f13dfb885236b728216376504f4b6c861ca83f2
Chris Lock <code@catharsis.co.uk>
2023-05-09 14:31:46 +0100

    Adding login via Auth0

--- src/components/MainTitle.vue
+++ src/components/MainTitle.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
 import { useAuth0 } from '@auth0/auth0-vue'
+import Navigation from './Navigation.vue'
 
 const auth0 = useAuth0()
-
 const isAuthenticated = auth0.isAuthenticated
-const isLoading = auth0.isLoading
-const login = () => {
-  auth0.loginWithRedirect()
-}
-const logout = () => {
-  auth0.logout({
-    logoutParams: {
-      returnTo: window.location.origin
-    }
-  })
-}
 const user = auth0.user
 </script>
 
@@ -24,20 +13,10 @@ const user = auth0.user
       <h1>Kids on Bikes Helper</h1>
     </div>
   </div>
-  <nav>
-    <ul>
-      <li v-if="!isAuthenticated && !isLoading">
-        <a @click="login">Login</a>
-      </li>
-      <li v-if="isAuthenticated">
-        <a @click="logout">Log Out</a>
-      </li>
-      <li v-if="isAuthenticated">
-        <router-link to="character" href="">Character</router-link>
-      </li>
-    </ul>
-  </nav>
+
+  <Navigation />
+
   <div v-if="isAuthenticated">
-    <h2>Welcome {{ user.email }}</h2>
+    <h2 data-testid="userEmail">Welcome {{ user.email }}</h2>
   </div>
 </template>
